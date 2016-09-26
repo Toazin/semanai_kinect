8d6954b3204e32f5c6935c9417ca1e363cf37161
toatzin <toatzin_2@hotmail.com>
2016-09-26 12:35:41 -0500

    random origins enemy

---
+++ Assets/Semanai/EnemyBehavior.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    public GameObject enemy;
+    IEnumerator frecAppear;
+    private int limLat, limSup;
+    //public GameObject player;
+    public List<GameObject> objectives; //objetivo final
+    private GameObject player;
+
+
+    // Use this for initialization
+    void Start () {
+        //player = GameObject.Find("CubeMan");
+        //Destroy(enemy, 10);
+        frecAppear = behavior();
+        StartCoroutine(frecAppear);
+        limLat = 12;
+        limSup = 7;
+        player = GameObject.Find("CubeMan");
+        foreach (Transform child in player.transform)
+        {
+            objectives.Add(child.gameObject);
+        }
+        foreach (GameObject name in objectives)
+        {
+            Debug.Log(name.name);
+        }
+        /*
+                foreach (Transform child in player.transform)
+                {
+                    Debug.Log(child.gameObject.name);
+                    objectives.Add(child.gameObject);
+                }
+                foreach (GameObject name in objectives)
+                {
+                    Debug.Log(name.name);
+                }
+                */
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+    IEnumerator behavior()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            
+
+            GameObject en1 = (GameObject)Instantiate(enemy, new Vector3(Random.Range(-1*limLat, limLat),
+                Random.Range(0, limSup)), transform.rotation);            
+
+            //en1.transform.Translate(0, Time.deltaTime * 10, 0);
+        }
+    }
+
+
+}
---
+++ Assets/Semanai/EnemyPref.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyPref : MonoBehaviour {
+
+    public int enemySpeed;
+    //public List<GameObject> objectives; //objetivo final
+    public GameObject player;
+
+                                       // Use this for initialization
+    void Start () {
+        
+        player = GameObject.Find("CubeMan");
+        /*
+        foreach (Transform child in player.transform)
+        {
+            objectives.Add(child.gameObject);
+        }
+        foreach(GameObject name in objectives)
+        {
+            //Debug.Log(name.name);
+        }
+        */
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //Debug.Log(objectives[Random.RandomRange(1, 15)].name);
+        transform.LookAt(player.transform);
+        transform.Translate(transform.forward * Time.deltaTime * enemySpeed, Space.World);
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        Destroy(gameObject);
+    }
+}
